83c2d04bbc8cde1bbeb7a6184f4c1d788d04d0a5
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-01-31 23:18:58 +0530

    Components

--- frontend/src/Components/InputBox.jsx
+++ frontend/src/Components/InputBox.jsx
@@ -2,8 +2,11 @@ function InputBox({
   label
 }) {
   return (
-    <div className="text-slate-500 text-md  pt-1 px-4 pb-4">
-      {label}
+    <div>
+      <div className="text-slate-500 text-md  pt-1 px-4 pb-4">
+        {label}
+      </div>
+      <input placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
     </div>
   )
 }
---
+++ frontend/src/Components/Warning.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Warning({
+  label
+}) {
+  return <div className="py-2 text-sm flex justify-center">
+    <div>
+      {label}
+    </div>
+    <Link className="pointer underline pl-1 cursor-pointer" to={to}>
+      {buttonText}
+    </Link>
+  </div>
+}
+
+
+export default Warning;
