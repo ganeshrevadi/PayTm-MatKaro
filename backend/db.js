ff172b2a4654d98a256780f77f4200fe780de7ad
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-02-10 15:31:43 +0530

    Env Variables

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+const dotenv = require('dotenv');
+dotenv.config()
 mongoose.connect(
-);
-
+  process.env.DB_URL);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
